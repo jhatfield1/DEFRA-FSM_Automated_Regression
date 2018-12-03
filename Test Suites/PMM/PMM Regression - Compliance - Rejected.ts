67ca0203ef88e7da3e65ca89bc10acaf534ed70a
JHATFIELD <JHATFIELD@SNDFSMINFSRV001.defra.onmicrosoft.com>
2018-12-03 12:48:33 +0000

    As of 03/12

---
+++ Test Suites/PMM/PMM Regression - Compliance - Rejected.ts
@@ -0,0 +1,160 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>R21 - FP-184</description>
+   <name>PMM Regression - Compliance - Rejected</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>42247f7c-a4ab-4686-950a-068bb5a2cfd0</testSuiteGuid>
+   <testCaseLink>
+      <guid>9ed4db28-cb8f-46ca-b9a7-1bb2410779cc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Generic/Launch Job Manager Control Centre</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>53f71fb4-eec6-43bd-8c17-d00a315937ac</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Pre Production/Environmental/UandP</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>53f71fb4-eec6-43bd-8c17-d00a315937ac</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Username</value>
+         <variableId>7e7a05b7-a8fe-4263-a211-ed7d0214de26</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>53f71fb4-eec6-43bd-8c17-d00a315937ac</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>b38ba976-6133-4f9c-9a6f-8f2b4255ea5d</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>2dc3ac28-c9f2-41f2-90d4-368b3d76d301</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>328953dc-c0f1-4286-af32-ebcb42aa9629</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/PMM/Compliance/Create PMM Inspection - Compliance</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6c2e7b75-af10-4ed2-a16f-84a0a2b1102e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Generic/Launch Job Manager</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>cb6b3d31-ba25-4643-94da-4e6b990d99d9</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Pre Production/Environmental/UandP</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>cb6b3d31-ba25-4643-94da-4e6b990d99d9</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Username</value>
+         <variableId>3d4f513b-8b02-4fb9-b880-2c2e951d8f69</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>cb6b3d31-ba25-4643-94da-4e6b990d99d9</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>13e7929c-4cc0-4343-bb83-7779b5b87a8c</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>cb6b3d31-ba25-4643-94da-4e6b990d99d9</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>JMUsername</value>
+         <variableId>2024dc9b-cb06-41b8-bb97-d017aea523ff</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>cb6b3d31-ba25-4643-94da-4e6b990d99d9</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>JMPassword</value>
+         <variableId>c378ba3c-5e46-49f4-ad39-a46ad361e174</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>b0509a92-379e-4540-8708-b866ec98cc71</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>30733adb-a31b-4f30-97bf-15b0e18a5727</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Generic/Select Inspection For Rejection - Conflict of Interest</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>bb4ea294-87ff-4a16-b97b-b6c68c7499bc</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dd906c3a-9ee0-4452-8c14-277012ddf1c0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Generic/Launch Job Manager Control Centre - Check Result</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>4181d4e9-2a7f-4df4-85f8-4628e111816f</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Pre Production/Environmental/UandP</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>4181d4e9-2a7f-4df4-85f8-4628e111816f</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Username</value>
+         <variableId>9633dd6d-8d38-4770-99c6-d4c4736ba3da</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>4181d4e9-2a7f-4df4-85f8-4628e111816f</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>9acaa269-a0a3-406d-bc7c-7ca8e7573278</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>647a4c54-3be1-4207-ae05-6f58ee7d6155</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>de4981aa-d198-4aec-a8ad-f3b61acde704</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ABP/Rejection/Control Centre - Check Results - Rejection</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>95a5c365-be5f-4d44-b459-13ead3db6d79</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b21dacbe-39d9-4340-b6bc-8f8d03483135</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ABP/Generic/Check Subsequent Inspection Doesnt Exist</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
